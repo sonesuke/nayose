92f4c22d267469f2ad5b48ed8d318c2d5f2ff9dc
sonesuke <iamsonesuke@gmail.com>
2020-03-15 18:03:48 +0900

    change data format to feather

--- tools/transform.py
+++ tools/transform.py
@@ -1,29 +1,34 @@
-import csv
-import re
+import pandas as pd
 
-state = set()
-city = set()
-with open('data/KEN_ALL.CSV', encoding="cp932") as f, open('test.csv', 'w') as g:
-    reader = csv.reader(f)
-    for row in reader:
-        s = row[6]
-        state.add(s)
-        m = re.match('(大和郡山市|小郡市|蒲郡市)(.*)', row[7])
-        if m:
-            c = m[1]
-            st = m[2] + row[8]
-        else:
-            m = re.match('(.+郡)(.+)', row[7])
-            c = m[1] if m else row[7]
-            st = m[2] + row[8] if m else row[8]
-        city.add(c)
-        full = row[6] + row[7] + row[8]
-        g.write(','.join([full, s, c, st]) + '\n')
+df = pd.read_csv('data/KEN_ALL.CSV', encoding="cp932", header=None, dtype=str)
 
-with open('states.txt', "w") as f:
-    for s in state:
-         f.write('"' + s + '",')
+df['Zip'] = df[2]
+df['State'] = df[6]
+df['Address'] = df[6] + df[7] + df[8]
 
-with open('cities.txt', "w") as f:
-    for c in city:
-        f.write('"' + c + '",')
+city_1 = df[7].str.extract('(大和郡山市|小郡市|蒲郡市)(.*)')
+df.loc[city_1[0].notnull(), 'City'] = city_1[0]
+df.loc[city_1[0].notnull(), 'Street'] = city_1[1].fillna('') + df[8]
+
+city_2 = df[7].str.extract('(.+郡)(.+)')
+index = df['City'].isnull()
+df.loc[index, 'City'] = city_2[0]
+df.loc[index, 'Street'] = city_2[1].fillna('') + df[8]
+
+index = df['City'].isnull()
+df.loc[index, 'City'] = df[7]
+df.loc[index, 'Street'] = df[8]
+
+df = df[['Zip', 'State', 'City', 'Street', 'Address']]
+
+
+def serialize(df, columns, file_path):
+    tgt = df[columns].drop_duplicates()
+    tgt = tgt.reset_index(drop=True)
+    tgt.to_feather(file_path)
+
+
+serialize(df, ['State'], 'state.ft')
+serialize(df, ['City'], 'city.ft')
+serialize(df, ['Address'], 'address.ft')
+serialize(df, ['State', 'City', 'Street', 'Address'], 'test.ft')
