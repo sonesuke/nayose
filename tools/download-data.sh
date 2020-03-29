6003abbc537af5aca66efd82344a3f01165713b9
sonesuke <iamsonesuke@gmail.com>
2020-03-29 17:08:00 +0900

    separate tool project

--- tools/download-data.sh
+++
@@ -1,7 +0,0 @@
-curl https://www.post.japanpost.jp/zipcode/dl/kogaki/zip/ken_all.zip -o data/ken_all.zip
-unzip -o data/ken_all.zip -d data
-rm data/ken_all.zip
-
-curl http://mobile.shinsv.mydns.jp/jinmei/ime-import.zip -o data/ime-import.zip
-unzip -o data/ime-import.zip -d data
-rm data/ime-import.zip
